3a6cf1f6625467948192558df1146c94e075fe2d
camilebron <camilebron@gmail.com>
2016-10-01 22:33:02 -0400

    Add files via upload

--- Frogger - Project 3/js/app.js
+++ Frogger - Project 3/js/app.js
@@ -3,40 +3,39 @@ var Enemy = function() {
     this.y = Math.random()*400;
     this.speed = this.getspeed();
     this.sprite = 'images/enemy-bug.png';
-    this.pos = this.x + this.y;
-    return this;
-
+    //this.pos = this.x + this.y;
+    //return this;
 };
 
 Enemy.prototype.getspeed = function(){
     var random = Math.random();
-    if (Math.floor(random) < 1)
-    {
-        this.speed = 3.5;
-    }
-    else
-    {
-        this.speed = 6;
-    }
+    this.speed = 200 * random;
     return this.speed;
 }
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
 
-    this.pos += this.speed * dt;
-    if (this.x >= 505 || this.y >= 606) {
-      reset();
+  this.x += this.speed * dt;
+  if(this.x>500){
+    this.x=0;
+  };
+  if(this.y > 375){
+    this.y=275;
+  } else if (this.y < 100) {
+    this.y=175;
     };
+  };
+  //function checkCollisions() {
+    //if (player.y === enemy.y) {
+      //this.reset();
+      //alert("YOU LOST!!");
+    //}
+    //else if (player.x === enemy.x){
+      //this.reset();
+      //alert("YOU LOST!!");
+  //};
 
-    if (this.y >= 400) {
-      this.y = 250;
-      return this.y;
-    };
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -47,53 +46,38 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-  for (var i = 0; i <= 3; i++) {
-    var enemy = new Enemy;
-    allEnemies.push(enemy);
-  };
 
-
-
+for (var i = 0; i <= 3; i++) {
+  var enemy = new Enemy;
+  allEnemies.push(enemy);
+};
 
 var Player = function() {
-      this.x = 253;
-      this.y = 600;
+      this.x = 200;
+      this.y = 425;
       this.sprite = 'images/char-horn-girl.png';
-
   };
 
 var player = new Player();
 
-Player.prototype.update = function() {
-    checkCollisions();
-    if (this.y == 0) {
-      this.reset();
-      alert("YOU WON!!");
-    };
-};
 
+Player.prototype.update = function() {
+
+      if (this.y == 0) {
+        this.reset();
+        alert("YOU WON!!");
+    };
+  };
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 
-function checkCollisions(){
-  if (player.y === enemy.y && player.x === enemy.x){
-    player.reset();
-    alert("YOU LOST!!");
-  };
-};
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -102,5 +86,19 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+
+   player.handleInput(allowedKeys[e.keyCode]);
 });
+
+Player.prototype.handleInput = function(keyCode) {
+  if (keyCode == 'up'){
+    this.x += 100;
+  } else if (keyCode == 'down') {
+    this.x -= 100;
+  } else if (keyCode == 'left') {
+    this.y -= 100;
+  } else (keyCode == 'right')
+    this.y += 100;
+  };
