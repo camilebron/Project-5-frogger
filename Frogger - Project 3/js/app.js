e388dad452cba7328f3a7d1aed5a806c210fb66e
camilebron <camilebron@gmail.com>
2016-10-13 13:04:40 -0400

    Add files via upload

--- Frogger - Project 3/js/app.js
+++ Frogger - Project 3/js/app.js
@@ -1,11 +1,51 @@
+var lanes = [60, 145, 230, 300];
+
+// Credits http://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 var Enemy = function() {
+  var number = getRandomInt(0, lanes.length-1);
     this.x = 0;
-    this.y = Math.random()*400;
+    this.y = lanes[number];
     this.speed = this.getspeed();
     this.sprite = 'images/enemy-bug.png';
-    //this.pos = this.x + this.y;
-    //return this;
+    this.width = 101;
+    this.height = 75;
 };
+function positionReset() { //if collision detected reset player to start position, if player reaches water alert you won, and position reset to start.
+  /*if(checkCollision = true || player.y = (range btween 0-87)) {
+  positionReset
+  }*/
+    player.x = 200;
+    player.y = 425;
+    //console.log (positionReset);
+};
+
+
+function checkCollisions() {
+
+    if (Enemy.x < Player.x + Player.width &&
+    Enemy.x + Enemy.width > Player.x &&
+    Enemy.y < Player.y + Player.height &&
+    Enemy.height + Enemy.y > Player.y) {
+        positionReset(); // collision detected. player returns to start position.
+        alert("YOU LOSE!");
+    };
+    //console.log(checkCollisions);
+  };
+
+
+Enemy.prototype.update = function(dt) {
+
+    this.x += this.speed * dt;
+    if(this.x>500){
+      this.x = 0;
+      this.y = lanes[getRandomInt(0, lanes.length-1)];//enemy changing lanes when reappearing
+    };
+    checkCollisions();
+  };
 
 Enemy.prototype.getspeed = function(){
     var random = Math.random();
@@ -14,27 +54,6 @@ Enemy.prototype.getspeed = function(){
 }
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-
-  this.x += this.speed * dt;
-  if(this.x>500){
-    this.x=0;
-  };
-  if(this.y > 375){
-    this.y=275;
-  } else if (this.y < 100) {
-    this.y=175;
-    };
-  };
-  //function checkCollisions() {
-    //if (player.y === enemy.y) {
-      //this.reset();
-      //alert("YOU LOST!!");
-    //}
-    //else if (player.x === enemy.x){
-      //this.reset();
-      //alert("YOU LOST!!");
-  //};
 
 
 // Draw the enemy on the screen, required method for game
@@ -57,9 +76,14 @@ for (var i = 0; i <= 3; i++) {
 };
 
 var Player = function() {
-      this.x = 200;
+      this.x = 200; //player star position
       this.y = 425;
       this.sprite = 'images/char-horn-girl.png';
+      this.width = 90;
+      this.height = 89;
+      //if(player.y >){ //limit players movements to canvas width ahd height
+
+      //}
   };
 
 var player = new Player();
@@ -71,6 +95,8 @@ Player.prototype.update = function() {
         this.reset();
         alert("YOU WON!!");
     };
+    checkCollisions();
+
   };
 
 Player.prototype.render = function() {
@@ -94,11 +120,11 @@ document.addEventListener('keyup', function(e) {
 
 Player.prototype.handleInput = function(keyCode) {
   if (keyCode == 'up'){
-    this.y -= 100;
+    this.y -= 87; // decrease
   } else if (keyCode == 'down') {
-    this.y += 100;
+    this.y += 87;
   } else if (keyCode == 'left') {
-    this.x += 100;
+    this.x -= 87;
   } else if (keyCode == 'right')
-    this.x -= 100;
-  };
+    this.x += 87;
+};
--- Frogger - Project 3/js/engine.js
+++ Frogger - Project 3/js/engine.js
@@ -111,11 +111,12 @@ var Engine = (function(global) {
                 'images/water-block.png',   // Top row is water
                 'images/stone-block.png',   // Row 1 of 3 of stone
                 'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
+                'images/stone-block.png',   // Row 3 of 4 of stone
+                'images/stone-block.png',   // Row 4 of 4 of stone
                 'images/grass-block.png',   // Row 1 of 2 of grass
                 'images/grass-block.png'    // Row 2 of 2 of grass
             ],
-            numRows = 6,
+            numRows = 7,
             numCols = 5,
             row, col;
 
